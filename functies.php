7dee1ab9bd96e9d40312c943bdc291db99e3d3e2
user <user@localhost>
2016-12-06 10:26:13 +0100

    Demonstratie

--- functies.php
+++ functies.php
@@ -10,3 +10,20 @@ if(preg_match("~([a-zA-Z0-9!#$%&amp;'*+-/=?^_`{|}~])@([a-zA-Z0-9-]).([a-zA-Z0-9]
         $email = "e-mail ongeldig"; //Maak de variabele leeg en zet er "email ongeldig" in
         return false;
 }}
+
+function verbinddatabase() {
+$servernaam = "localhost";
+$username = "root";
+$password = "Admin01!";
+$database = "fts";
+    
+// Maak connectie
+$conn = new mysqli($servernaam, $username, $password, $database);    
+    
+// Check connection
+if ($conn->connect_error) {
+    die("Verbindingsfout!: " . $conn->connect_error);
+} else{
+return true;
+}}
+
