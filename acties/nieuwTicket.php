60079e9ba9d3b7e41b78886c625be7c2b265c571
user <user@localhost>
2016-12-06 11:24:14 +0100

    Demonstratie

--- acties/nieuwTicket.php
+++ acties/nieuwTicket.php
@@ -5,4 +5,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once 'functies.php';
+require_once 'header.php';
 
+verbinddatabase();
+
+?>
