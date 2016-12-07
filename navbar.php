3c7f428d1cf78f83352c39389be7d79d234ce145
user <user@localhost>
2016-12-07 11:42:01 +0100

    start html

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <ul>
   <li><a class="active" href="#home">Dash</a></li>
   <li><a href="#news">Tickets</a></li>
-  <li style="float:right"><a href="#about">Nieuw ticket</a></li>
+  <li style="float:right"><a href="#about">+ Nieuw ticket</a></li>
 </ul>
 
