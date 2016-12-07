630dfe3c547e12cd92df3904a6eb55b89235eeae
user <user@localhost>
2016-12-07 13:41:45 +0100

    start html

--- tickets.php
+++ tickets.php
@@ -13,7 +13,17 @@ include_once 'functies.php';
   <link rel="stylesheet" href="Styles.css" type="text/css">
   </head>
   <body>
-  
+      
+      <h2> Gevanceerd sorteren </h2>
+      <form name="filterfunctie" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+          Naam <br>
+          <input type="text" name="Titel"><br>
+          Beschrijving <br>
+          <input type="text" name="Beschrijving"><br>
+          <input type="submit" name="zoeken" value="zoeken"><br>    
+      </form>
+      
+      
   </body>
 </html>
 
