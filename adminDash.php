3ebc414775c4c8982034591f1518eafbbaea6550
user <user@localhost>
2016-12-07 14:38:31 +0100

    start html

--- adminDash.php
+++ adminDash.php
@@ -4,9 +4,12 @@ require_once 'header.php'; // Zet de header bovenaan deze pagina.
 ?>
 
 <form name="nieuwaccount" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-          Naam <br>
-          <input type="text" name="Naam"><br>
-          Beschrijving <br>
-          <input type="text" name="Wachtwoord"><br>
+          voornaamaam <br>
+          <input type="text" name="voornaam"><br>
+          achternaam <br>
+          <input type="text" name="acternaam"><br>
+          Klas <br>
+          <input type="text" name="klas"><br>
+
           <input type="submit" name="zoeken" value="zoeken"><br>    
       </form>
