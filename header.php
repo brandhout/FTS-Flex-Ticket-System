68f83ee81c8449276d149b6ebd2bc7fb5f79e9c3
user <user@localhost>
2016-12-07 15:03:06 +0100

    start html

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <head>
     <meta charset="UTF-8">
-    <title>Tickets</title>
   <link rel='stylesheet' href='//fonts.googleapis.com/css?family=font1|font2|etc' type='text/css'>
   <link rel="stylesheet" href="Styles.css" type="text/css">
   </head>
