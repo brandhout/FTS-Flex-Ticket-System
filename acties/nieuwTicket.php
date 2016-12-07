97560c23e5e4c94a86685053e0206b3d5234c97e
user <user@localhost>
2016-12-07 15:04:01 +0100

    start html

--- acties/nieuwTicket.php
+++ acties/nieuwTicket.php
@@ -5,11 +5,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-require_once 'functies.php'; //Include de functies.
-require_once 'header.php'; // Zet de header bovenaan deze pagina.
+require_once '../functies.php'; //Include de functies.
+require_once '../header.php'; // Zet de header bovenaan deze pagina.
 
 verbinddatabase();
-
+//$fstAccountNr= Moet gekoppeld worden aan momenteel ingelogde account
 $probleem=NULL;
 $trefwoorden=NULL;
 $aantalXterug=NULL;
@@ -30,5 +30,17 @@ $besturingssysteem="standaard";
 ?>
 <h1> Test </h1>
 <form name="nieuwTicket" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    
+          Probleem (korte omschrijving) <br>
+          <input type="text" name="Beschrijving"><br>
+
+          trefwoorden (aan elkaar, door komma gescheiden ) <br>
+          <input type="text" name="Beschrijving"><br>
+
+          Probleem (korte omschrijving) <br>
+          <input type="text" name="Beschrijving"><br>
+
+          <input type="checkbox" name="vehicle" value="Bike">Klant moet nog gebeld worden<br>
+    
 <input type="submit" name="invoeren" value="invoeren"><br>    
 </form>
