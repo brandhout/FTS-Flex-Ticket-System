e7d1a3753809d1ee25facad5c5b0993000bbe7ad
user <user@localhost>
2016-12-07 11:47:28 +0100

    start html

--- acties/nieuwTicket.php
+++ acties/nieuwTicket.php
@@ -11,3 +11,7 @@ require_once 'header.php'; // Zet de header bovenaan deze pagina.
 verbinddatabase();
 
 ?>
+<h1> Test </h1>
+<form name="nieuwTicket" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<input type="invoeren" name="invoeren" value="invoeren"><br>    
+</form>
