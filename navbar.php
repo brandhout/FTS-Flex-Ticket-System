741f2445f4e2ded9d63d57e101d5289572778366
user <user@localhost>
2016-12-08 13:59:27 +0100

    v. 0.1.4 db knop gemaakt v. 0.1.5 menu beginstaat v. 0.1.6 50% menu

--- navbar.php
+++ navbar.php
@@ -5,24 +5,24 @@
   <li><a href="#">Uitloggen</a></li>  
   <li><a href="tickets.php">Tickets</a></li>
 
-      <?php
+    <?php
 /*
  * De volgende switch kijkt of de variabele isAdmin TRUE of FALSE is. Als hij true
  * is dan komt er een extra veld bij de navbar. De adminDash kan dan geopend worden.
  * Normaal doet hij niets, ook als de variabele dus NULL is.
  */
- switch ($isAdmin) { 
-    case "TRUE":
-        echo '<li><a href="adminDash.php">administrator</a></li>' ;
-        break;
+        switch ($isAdmin) { 
+            case "TRUE":
+            echo '<li><a href="adminDash.php">administrator</a></li>' ;
+            break;
     
-    case "FALSE":
-        break;
+            case "FALSE":
+            break;
     
-    default:
-        break;
-}
-?>
+            default:
+            break;
+        }
+    ?>
   
   <li><a href="acties/nieuwTicket.php">+ Nieuw ticket</a></li>
 </ul>
@@ -30,8 +30,9 @@
 
 
  <!-- database knop -->
- <div class="center">
-<a href="#" class="dbknop">Dashboard</a>
- </div>
+<div class="center">
+    <a href="#" class="dbknop">Dashboard</a>
+</div>
+ <br><br><br>
 
 </html>
