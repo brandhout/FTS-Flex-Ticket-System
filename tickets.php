2b9e09c47ce0c9f49fe1d360b697e6776e6ebb1d
rhuijzer <rhuijzer@localhost>
2016-12-10 17:54:41 +0100

    Begin Loop die alle tickets uit DB harkt

--- tickets.php
+++ tickets.php
@@ -2,10 +2,38 @@
 
 include_once 'header.php';
 include_once 'functies.php';
+verbinddatabase();
 
+$query= "";
+$uitkomst= mysqli_query($connectie, $query)
+        or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
 
+if($uitkomst) {
+// Maak HTML tabel!
 
-
+    echo '<table align="left"
+        <td><td align="left"><strong>TicketID</strong></td>
+        <td><td align="left"><strong>Onderwerp</strong></td>
+        <td><td align="left"><strong>Klantnaam</strong></td>
+        <td><td align="left"><strong>Lijn</strong></td>
+        <td><td align="left"><strong>Aannemer</strong></td>
+        <td><td align="left"><strong>Opgelosd</strong></td></tr>';
+    
+    while($row = mysqli_fetch_array($uitkomst)){
+        
+        echo '<tr><td align=left">' .
+                $row[ticketId] . '</td><td align="left">' .
+                $row[onderwerp] . '</td><td align="left">' .
+                $row[klantAchterNaam] . '</td><td align="left">' .
+                $row[lijnNr] . '</td><td align="left">' .
+                $row[fstAccountNr] . '</td><td align="left">' .
+                $row[definitief] . '</td><td align="left">';
+        
+        echo '</tr>';
+    }
+    echo '</table>';
+    
+}
  ?>
 
  <!DOCTYPE html>
