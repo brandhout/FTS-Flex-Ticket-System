f2ccf053e1e7b1dd74636c9a7d470ce9f813ccd3
rhuijzer <rhuijzer@localhost>
2016-12-10 19:49:35 +0100

    Croissantje hagelslag

--- tickets.php
+++ tickets.php
@@ -19,20 +19,20 @@ if($uitkomst) {
         <td><td align="left"><strong>Aannemer</strong></td>
         <td><td align="left"><strong>Opgelosd</strong></td></tr>';
     
-    while($row = mysqli_fetch_array($uitkomst)){
-        
+    while($rij = mysqli_fetch_array($uitkomst)){
+        // Deze 'zolang' of 'terwijl' loop geeft een rij data, en herhaalt dit totdat er geen data
+        // meer in de uitkomst zit.
         echo '<tr><td align=left">' .
-                $row[ticketId] . '</td><td align="left">' .
-                $row[onderwerp] . '</td><td align="left">' .
-                $row[klantAchterNaam] . '</td><td align="left">' .
-                $row[lijnNr] . '</td><td align="left">' .
-                $row[fstAccountNr] . '</td><td align="left">' .
-                $row[definitief] . '</td><td align="left">';
+                $rij[ticketId] . '</td><td align="left">' .
+                $rij[onderwerp] . '</td><td align="left">' .
+                $rij[klantAchterNaam] . '</td><td align="left">' .
+                $rij[lijnNr] . '</td><td align="left">' .
+                $rij[fstAccountNr] . '</td><td align="left">' .
+                $rij[definitief] . '</td><td align="left">';
         
         echo '</tr>';
     }
-    echo '</table>';
-    
+    echo '</table>';    
 }
  ?>
 
