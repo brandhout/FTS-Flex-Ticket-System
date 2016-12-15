a468c56764bca593dd17d0af8663a3241d3e4de0
user <user@localhost>
2016-12-15 14:01:42 +0100

    v. 16.12.16 verbeterd

--- acties/nieuwTicket.php
+++ acties/nieuwTicket.php
@@ -34,9 +34,17 @@ $factuurNr=NULL;
 <!DOCTYPE html>
 <html>
 <h1> Nieuw ticket </h1>
+
 <body>
- <div class="algemeen1">
+    
+ <div class="form1">  
      <form name="nieuwTicket" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+<<<<<<< HEAD
+         
+<div class="a1">
+          <p>Bestaande klant <!-- Moet uit database komen!! -->
+          <select name="dag">
+=======
  <div class="a1">  
           trefwoorden (aan elkaar, door komma gescheiden) <br> <!-- Afwijkende gegevensfilter. Trefwoorden moeten in kommagescheiden Array -->
           <input type="text" name="trefwoorden"><br><br>
@@ -48,45 +56,46 @@ $factuurNr=NULL;
           
           Bestaande klant <br> <!-- Moet uit database komen!! -->
           <select name="dag" id="bestaandeKlant">
+>>>>>>> origin/master
           <option>Herman</option>
           <option>Milad</option>
-          </select><br><br>
-          
-          <input type="checkbox" name="nieuwKlant" value="nieuwKlant">Nieuwe klant<br><br>
-         <!-- Als nieuwe klant aangevinkt is dan kunnen NAW gegevens ingevuld worden -->
-          Nieuwe klant (Achternaam) <br>
-          <input type="text" name="klantAchterNaam" disabled><br><br>
-          
-          Nieuwe klant (Voornaam) <br>
-          <input type="text" name="klantNaam" disabled><br><br>
+          </select>
+          Nieuwe klant<input type="checkbox" name="nieuwKlant" value="nieuwKlant"></p></div>
+         <!-- Als nieuwe klant aangevinkt is dan kunnen NAW gegevens ingevuld worden -->         
+<div class="a2">  
+    <p>trefwoorden (aan elkaar, door komma gescheiden)<!-- Afwijkende gegevensfilter. Trefwoorden moeten in kommagescheiden Array -->
+        <input type="text" name="trefwoorden"></p></div>
+        
+ <div class="a3">            
+          <p>streefdatum: <input type="date" id="datepicker"></p>
+</div>
 
-          Nieuwe klant (Adres) <br>
-          <input type="text" name="klantAdres" disabled><br><br>
-          
-          Nieuwe klant (Postcode) <br>
-          <input type="text" name="klantPostc" disabled><br><br>
-
-          Nieuwe klant (Woonplaats) <br>
-          <input type="text" name="klantStad" disabled><br><br>
-
-          
-          <input type="checkbox" name="nogBellen" value="nogBellen">Klant moet nog gebeld worden</div><br><br>
-          
-    <div class="a3">     
-          <h3> Categorieën </h3>
-          
-          Categorie <!-- Moet uit database komen -->
+<div class="a1">            
+     <p>Nieuwe klant (Voornaam)
+         <input type="text" name="klantNaam" disabled></p></div>
+         
+    <div class="a2">     
+        <p>Categorie <!-- Moet uit database komen -->
           <select name="categorie" disabled>
               <option>Software</option>
               <option>Hardware</option>
-          </select><br><br>
-          
-          Subcategorie <!-- Disabled, voor later. -->
+          </select><br>
+                  Subcategorie <!-- Disabled, voor later. -->
           <select name="subCategorie" disabled>
               <option>Fedora Linux</option>
               <option></option>
-          </select><br>
+          </select></p></div>
+<div class="a3">           
+      <p>telefoonnummer klant:<input type="text" name="klantTel">
+          Klant moet nog gebeld worden:<input type="checkbox" name="nogBellen" value="nogBellen">
+          </p>
+</div>
 
+<<<<<<< HEAD
+<div class="a1">
+    <p>Nieuwe klant (Achternaam)
+        <input type="text" name="klantAchterNaam" disabled></p></div>
+=======
           
           <h3> Streefdatum</h3>
                     
@@ -110,45 +119,69 @@ $factuurNr=NULL;
               <option>Telefoon</option>
               <option>E-mail</option>
           </select><br><br>
+>>>>>>> origin/master
 
-                     Lokatie: 
-              <br><select name="binnenkomstType" disabled><br> <!-- Disabled, gaan we nog niets mee doen-->
-              <option>Hilversum Soestdijkerstraatweg</option>
-              <option></option>
-          </select><br>
-
-          <h3>Veelvoorkomende laptop:</h3><br> <!-- Disabled, weinig tijd -->
-          Merk
+ <div class="a2">  
+          <p> <!-- Disabled, weinig tijd -->
+          Merk:
           <select name="vVLaptopMerk" disabled>
               <option></option>
               <option></option>
-          </select><br><br>
-          
-          Type
+          </select>     
+      Type
           <select name="vVLaptopType" disabled>
               <option></option>
               <option></option>
-          </select><br><br>
-          
-          Besturingssysteem
+          </select></p></div>
+<div class="a3">
+    <p>ontvangen via:
+          <select name="binnenkomstType" disabled> <!-- Moet nog gescript worden! Data moet uit database komen -->
+              <option>Telefoon</option>
+              <option>E-mail</option>
+          </select><br>
+     Lokatie: 
+              <select name="binnenkomstType" disabled> <!-- Disabled, gaan we nog niets mee doen-->
+              <option>Hilversum Soestdijkerstraatweg</option>
+              <option>uy</option>
+              </select></p>
+</div>
+
+<div class="a1">  
+     <p>Nieuwe klant (Adres)
+         <input type="text" name="klantAdres" disabled></p></div>
+  <div class="a2">            
+     <p>Besturingssysteem
           <br><select name="besturingssysteem" disabled>
               <option>Windows</option>
               <option></option>
-          </select><br><br></div>
+          </select></p></div>   
+         <div class="a3"> 
+     <p>Probleem (korte omschrijving)
+         <textarea id="probleem"></textarea></p></div>
          
-          <div class="a5">
-          <h3> Potentiele oplossing </h3>
-            <textarea id="oplossing" rows="10" cols="90"></textarea><br><br>
-          
-           <h3> Commentaar </h3>
-            <textarea id="nieuwComment" rows="10" cols="90"></textarea><br><br>
+ <div class="a1">            
+     <p>Nieuwe klant (Postcode)
+         <input type="text" name="klantPostc" disabled></p></div>
 
+ <div class="a2">            
+           <p> Commentaar
+               <textarea id="nieuwComment"></textarea></p>
             </div>
-<div class="a6">
-    <input type="submit" name="submit" value="invoeren"><br></div><br><br><br>    
+          <div class="a3">
+          <p> Potentiele oplossing
+              <textarea id="oplossing"></textarea></p></div>    
+         
+ <div class="a1">  
+     <p>Nieuwe klant (Woonplaats)</p>
+          <input type="text" name="klantStad" disabled></div>
+       
+<div class="a3">
+    <input type="submit" name="submit" value="invoeren"></div>   
 </form>
  </div>
-</body></html>
+
+</body>
+</html>
 
 <?php
 
--- header.php
+++ header.php
@@ -5,6 +5,16 @@
   <link rel="stylesheet" href="../styles.css" type="text/css"> <!-- Tijdelijke oplossing voor het includen van de style in hogere mappen -->
   <script src="navbar.js"></script>
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+   <!--datepicker -->
+     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script>
   </head>
 <header>
   <img class="logo" src="fts.PNG">
