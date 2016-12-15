f54185330759505e551e62d007e00ab878b767e2
user <user@localhost>
2016-12-15 14:52:36 +0100

    HTML fix

--- acties/nieuwTicket.php
+++ acties/nieuwTicket.php
@@ -24,18 +24,56 @@ $vVlaptopType=NULL;
 $besturingssysteem="standaard";
 $factuurNr=NULL;
 
+    
+if (!$_POST['submit'] === "") {
+    
     $leesKlantQuery= 'select * from klant';
     $klantenLijst= mysqli_query($connectie, $leesKlantQuery)
         or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
+
     
+    $uitkomst= mysqli_query($connectie, $ticketQuery)
+        or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
     
+        if (isset($_POST['nieuweKlant'])) {
+                $nieuweKlantQuery = "insert into klant klantAchterNaam = $klantAchterNaam,
+                klantNaam = $klantNaam, klantTel = $klantTel, klantAdres = $klantAdres, klantPostc = $klantPostc,
+                klantStad = $klantStad, klantEmail = $klantEmail";
+    
+
+
+        }
+        
+        if (isset($_POST['bestaandeKlant'])) {
+                $ticketQuery = "insert into ticket (fstAccountNr = $fstAccountNr, inBehandeling = TRUE, 
+                probleem = $probleem, trefwoorden = $trefwoorden, klantId = $klantId, prioriteit = $prioriteit,
+                aantalXterug = NULL terugstuurLock = FALSE, lijnNr = $lijnNr, datumAanmaak = $datumAanmaak,
+                nogBellen = $nogBellen, categorieNaam = $categorieNaam, factuurNr = $factuurNr,
+                log = $log, verlopen = $verlopen, streefdatum = $streefdatum,
+                lokatie = $lokatie, klantTevreden = $klantTevreden"; 
+                
+                $uitkomst= mysqli_query($connectie, $ticketQuery)
+                or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
+
+
+        }
+
+        
+        
+
+    
+}  
 
 ?>
 <!DOCTYPE html>
 <html>
+
+    <body>
+<h1> Nieuw ticket </h1>
+
+
     
-<body>
-    <div class="form1">
+ <div class="form1">
      
      <form name="nieuwTicket" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
          
@@ -58,13 +96,37 @@ $factuurNr=NULL;
         <div class="a3">           
             <p>telefoonnummer klant:<input type="text" name="klantTel">
             Klant moet nog gebeld worden:<input type="checkbox" name="nogBellen" value="nogBellen">
-            </p>
-        </div>
+          </p>
+</div>
 
 
-        <div class="a1">
-            <p>Nieuwe klant (Achternaam)
-            <input type="text" name="klantAchterNaam" disabled></p></div>
+<div class="a1">
+    <p>Nieuwe klant (Achternaam)
+        <input type="text" name="klantAchterNaam" disabled></p></div>
+
+          
+          <h3> Streefdatum</h3>
+                    
+                Dag <br>
+                <select name="dag" disabled>
+                <option>1</option>
+                <option>2</option>
+                </select><br><br>
+          
+                Maand <br>
+                <select name="maand" disabled>
+                <option>Januari</option>
+                <option>Februari</option>
+                </select><br><br>
+          
+                Jaar (2017) <br>
+                <input type="text" name="jaar" disabled></div><br><br>
+<div class="a4">
+                     Binnenkomst type: 
+          <select name="binnenkomstType" disabled> <!-- Moet nog gescript worden! Data moet uit database komen -->
+              <option>Telefoon</option>
+              <option>E-mail</option>
+          </select><br><br>
 
 
  <div class="a2">  
@@ -125,47 +187,6 @@ $factuurNr=NULL;
     <input type="submit" name="submit" value="invoeren"></div>   
 
  </div>
+
 </body>
-
 </html>
-
-<?php
-
-    
-    
-
-if (isset($_POST['submit'])) {
-    
-    $uitkomst= mysqli_query($connectie, $ticketQuery)
-        or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
-    
-        if (isset($_POST['nieuweKlant'])) {
-                $nieuweKlantQuery = "insert into klant klantAchterNaam = $klantAchterNaam,
-                klantNaam = $klantNaam, klantTel = $klantTel, klantAdres = $klantAdres, klantPostc = $klantPostc,
-                klantStad = $klantStad, klantEmail = $klantEmail";
-    
-
-
-        }
-        
-        if (isset($_POST['bestaandeKlant'])) {
-                $ticketQuery = "insert into ticket (fstAccountNr = $fstAccountNr, inBehandeling = TRUE, 
-                probleem = $probleem, trefwoorden = $trefwoorden, klantId = $klantId, prioriteit = $prioriteit,
-                aantalXterug = NULL terugstuurLock = FALSE, lijnNr = $lijnNr, datumAanmaak = $datumAanmaak,
-                nogBellen = $nogBellen, categorieNaam = $categorieNaam, factuurNr = $factuurNr,
-                log = $log, verlopen = $verlopen, streefdatum = $streefdatum,
-                lokatie = $lokatie, klantTevreden = $klantTevreden"; 
-                
-                $uitkomst= mysqli_query($connectie, $ticketQuery)
-                or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
-
-
-        }
-
-        
-        
-
-    
-}  
-
-?>
