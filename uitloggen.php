091e0065cdc05ac3b70210e1392f4a0b2d8b2dbc
user <user@localhost>
2016-12-16 10:08:55 +0100

    v. 16.12.16 uitloggen added

---
+++ uitloggen.php
@@ -0,0 +1,24 @@
+<html>
+
+    
+    <body>
+<header>
+  <img class="logo2" src="fts.PNG">
+</header>
+
+
+
+
+
+<?php
+
+session_start();
+unset ($_SESSION['Leerlingnummer']);
+session_destroy();
+echo 'u bent uitgelogd';
+header("Refresh:3 ;  URL=inloggen.php");
+?>
+
+        
+    </body>
+</html>
