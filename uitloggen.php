93a2b1e18b419ed68bafc4c1b92e46fd8524a5a6
user <user@localhost>
2016-12-20 11:14:51 +0100

    header is aangepast, niet op letten!

--- uitloggen.php
+++ uitloggen.php
@@ -2,23 +2,25 @@
 
     
     <body>
-<header>
-  <img class="logo2" src="fts.PNG">
-</header>
+        <header>
+          <img class="logo2" src="fts.PNG">
+        </header>
 
-
-
-
+    </body>
+</html>
 
 <?php
 
 session_start();
 unset ($_SESSION['gebruikersNaam']);
+
 session_destroy();
-echo 'u bent uitgelogd';
-header("Refresh:3 ;  URL=inloggen.php");
+echo" u bent uitgelogd";
+//echo ($_POST['gebruikersNaam'] . "is uitgelogd");
+
+header("Refresh:3 ;  URL=acties/inloggen.php");
+
 ?>
 
         
-    </body>
-</html>
+   
