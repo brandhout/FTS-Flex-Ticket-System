3be10a4453d8927d4e2459166bb64ecf547b4a68
rhuijzer <rhuijzer@localhost>
2016-12-21 17:15:13 +0100

    Implementatie: magInloggen, isAdmin, correct mysqli en cleanup

--- acties/inloggen.php
+++ acties/inloggen.php
@@ -9,28 +9,24 @@
 
     if(isset($_POST['gebruikersNaam']) && isset($_POST['wachtwoord'])){
         //verkrijg de variabele uit het forum hieronder, de functies voorkommen SQL injectie
-        $gebruikersNaam= stripcslashes(trim($_POST['gebruikersNaam']));
-        $wachtwoord = stripcslashes(trim($_POST['wachtwoord']));
-
-        $query = mysqli_query($connectie, "SELECT gebruikersNaam, wachtwoord FROM account WHERE gebruikersNaam = '$gebruikersNaam'");
-            //or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
+        $gebruikersNaam = mysqli_real_escape_string($connectie, stripcslashes(trim($_POST['gebruikersNaam'])));
+        $wachtwoord = mysqli_real_escape_string($connectie, stripcslashes(trim($_POST['wachtwoord'])));
+   
+        $query = mysqli_query($connectie, "SELECT gebruikersNaam, wachtwoord, isAdmin, magInloggen FROM account WHERE gebruikersNaam = '$gebruikersNaam'");
         $uitkomst = mysqli_fetch_array($query);
         $teller = mysqli_num_rows($query);
 
-        if ($teller == 1 && $uitkomst['wachtwoord'] == $wachtwoord){ //Als gegevens in de database gelijk zijn aan ingevulde gegevens
+        if ($teller == 1 && $uitkomst['wachtwoord'] == $wachtwoord && $uitkomst['magInloggen'] == 1){ //Als gegevens in de database gelijk zijn aan ingevulde gegevens
 
             // Inloggen succes, hier moet een sessie aangemaakt worden
             session_start();
-            $_SESSION["gebruikersNaam"] = "gebruikersNaam"; //etc etc
-            echo " sessie gestart";
+            $_SESSION["gebruikersNaam"] = "gebruikersNaam";
+            $_SESSION["isAdmin"] = $uitkomst['isAdmin'];
             header('Location: ../index.php'); 
-
-            } else { //Als de gevevens niet gelijk zijn
-
-                echo"Inloggen mislukt, kloppen uw gegevens?"; //Variabele met foutmelding wordt aangemaakt.
-            }   
-    
-    }
+            
+        } else {
+            return FALSE; 
+        }}
 ?>
 <html>    
     <head>
@@ -49,13 +45,8 @@
            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
                 <input id="inlog1" type="text" name="gebruikersNaam" required placeholder="Vul hier uw gebruikersnaam in*"><span id="message1" ></span><br>
                 <input type="password" name="wachtwoord" required placeholder="Vul hier uw wachtwoord in*"><span id="message2" ></span><br>
-                <input type="submit" value="Submit" onclick="checkinlog()">                  
+                <input type="submit" value="Submit">                  
             </form>
-            <?php 
-                if (!$foutmelding == "") {                
-                    echo $foutmelding; //Als foutmelding niet leeg is word hij weergegeven
-                }
-            ?>
         </div>
                 
     </body>
