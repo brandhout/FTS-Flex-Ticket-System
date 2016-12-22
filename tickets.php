87ee72cc18da350c282a4eaaa9cb19b236427972
user <user@localhost>
2016-12-22 12:55:35 +0100

    Code mooi maken voor Naomi haar OCD!

--- tickets.php
+++ tickets.php
@@ -1,41 +1,41 @@
 <?php
 
-include_once 'header.php';
-include_once 'functies.php';
-//verbinddatabase();
-session_start();
+    include_once 'header.php';
+    include_once 'functies.php';
+    $connectie = verbinddatabase();
+    session_start();
 
 
-if($uitkomst) {
-// Maak HTML tabel!
-    $query= "SELECT * from ticket";
-    $uitkomst= mysqli_query($connectie, $query)
-        or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
+    if($uitkomst) {
+    // Maak HTML tabel!
+        $query= "SELECT * from ticket";
+        $uitkomst= mysqli_query($connectie, $query)
+            or die("Kan aangevraagde actie niet verwerken:" .mysql_error());
 
 
-    echo '<table align="left"
-        <td><td align="left"><strong>TicketID</strong></td>
-        <td><td align="left"><strong>Onderwerp</strong></td>
-        <td><td align="left"><strong>Klantnaam</strong></td>
-        <td><td align="left"><strong>Lijn</strong></td>
-        <td><td align="left"><strong>Aannemer</strong></td>
-        <td><td align="left"><strong>Opgelosd</strong></td></tr>';
-    
-    while($rij = mysqli_fetch_array($uitkomst)){
-        // Deze 'zolang' of 'terwijl' loop geeft een rij data, en herhaalt dit totdat er geen data
-        // meer in de uitkomst zit.
-        echo '<tr><td align=left">' .
-                $rij[ticketId] . '<a href="acties/leesTicket.php?ticket='.$rij[ticketId].'></td><td align="left"></a>' . 
-                $rij[onderwerp] . '</td><td align="left">' .
-                $rij[klantAchterNaam] . '</td><td align="left">' .
-                $rij[lijnNr] . '</td><td align="left">' .
-                $rij[fstAccountNr] . '</td><td align="left">' .
-                $rij[definitief] . '</td><td align="left">';
-        
-        echo '</tr>';
+        echo '<table align="left"
+            <td><td align="left"><strong>TicketID</strong></td>
+            <td><td align="left"><strong>Onderwerp</strong></td>
+            <td><td align="left"><strong>Klantnaam</strong></td>
+            <td><td align="left"><strong>Lijn</strong></td>
+            <td><td align="left"><strong>Aannemer</strong></td>
+            <td><td align="left"><strong>Opgelosd</strong></td></tr>';
+
+        while($rij = mysqli_fetch_array($uitkomst)){
+            // Deze 'zolang' of 'terwijl' loop geeft een rij data, en herhaalt dit totdat er geen data
+            // meer in de uitkomst zit.
+            echo '<tr><td align=left">' .
+                    $rij[ticketId] . '<a href="acties/leesTicket.php?ticket='.$rij[ticketId].'></td><td align="left"></a>' . 
+                    $rij[onderwerp] . '</td><td align="left">' .
+                    $rij[klantAchterNaam] . '</td><td align="left">' .
+                    $rij[lijnNr] . '</td><td align="left">' .
+                    $rij[fstAccountNr] . '</td><td align="left">' .
+                    $rij[definitief] . '</td><td align="left">';
+
+            echo '</tr>';
+        }
+        echo '</table>';    
     }
-    echo '</table>';    
-}
  ?>
 
  <!DOCTYPE html>
