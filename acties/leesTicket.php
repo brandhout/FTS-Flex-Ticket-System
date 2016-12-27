268c72f9901ab62180909f4e6d8880f10fdcd83c
rhuijzer <rhuijzer@localhost>
2016-12-27 12:49:13 +0100

    Nettere code, ticket wordt nu ingelezen, query mist.

--- acties/leesTicket.php
+++ acties/leesTicket.php
@@ -1,35 +1,47 @@
 <?php
+    session_start();
+    require_once 'headerUp.php'; //Include de functies.
+    require_once '../functies.php'; //Include de functies.
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $connectie = verbinddatabase();
 
-/* 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    /* 
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
 
-require_once '../functies.php'; //Include de functies.
-require_once 'headerUp.php'; //Include de header.
+    require_once '../functies.php'; //Include de functies.
+    require_once 'headerUp.php'; //Include de header.
 
+    if (is_numeric($_GET['ticket'])) {
+        $ticketNr = $_GET['ticket'];
+    }
 
-
+    echo '
+        <!DOCTYPE html>
+        <html>
+        <h1> Ticketinfo </h1>
+        <h3> Ticketnummer: '. $ticketNr . ' </h3><br>
+        <body>
+        '
 ?>
 
 <!-- Als je op tickets.php een ticket opent, komt de ticket op deze pagina te staan, met de informatie uit de database.
 Je kan hier ook nieuw commentaar aanmaken, en oplossingen aandragen. Door de eerste behandelaar kan die definitief verklaard
 worden. Er kan dus aan een ticket meerdere stukken commentaar en oplossingen hangen! Mocht de echte ticket aangepast worden
 wordt de gebruiker doorgestuurd naar wijzigTicket.php. Dit alleen als er bijvoorbeeld een fout gemaakt is.-->
-<!DOCTYPE html>
-<html>
-<h1> Ticketinfo </h1>
-<body>
 <form name="leesTicket" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
     
           trefwoorden (aan elkaar, door komma gescheiden) <br> <!-- Afwijkende gegevensfilter. Trefwoorden moeten in kommagescheiden Array -->
