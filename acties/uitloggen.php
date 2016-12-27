8e432fa719d574229f8fdfa682d3584217bf2f9f
rhuijzer <rhuijzer@localhost>
2016-12-27 12:46:54 +0100

    Verplaatst. Is een actie.

---
+++ acties/uitloggen.php
@@ -0,0 +1,26 @@
+<html>
+
+    
+    <body>
+        <header>
+          <img class="logo2" src="fts.PNG">
+        </header>
+
+    </body>
+</html>
+
+<?php
+
+session_start();
+unset ($_SESSION['gebruikersNaam']);
+
+session_destroy();
+echo" u bent uitgelogd" . $uitkomst['gebruikersNaam'];
+//echo ($_POST['gebruikersNaam'] . "is uitgelogd");
+
+header("Refresh:2 ;  URL=acties/inloggen.php");
+
+?>
+
+        
+   
