2a08733bcebc00273f1d048923522624b6b44f7f
rhuijzer <rhuijzer@localhost>
2017-01-02 12:19:55 +0100

    TODO lijst

--- adminDash.php
+++ adminDash.php
@@ -1,7 +1,29 @@
 <?php
-session_start();
-require_once 'functies.php'; //Include de functies.
-require_once 'header.php'; // Zet de header bovenaan deze pagina.
+    /* 
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+
+    /*
+     * TODO:
+     * - Naamgeving onderstaande rechtrekken met database
+     * - Logica van onderstaande realiseren (query's etc)
+     * - Klanten toevoegen
+     * - Laptops toevoegen
+     */
+    session_start();
+    require_once 'functies.php'; //Include de functies.
+    require_once 'header.php'; // Zet de header bovenaan deze pagina.
 ?>
 <html>
     <header>
