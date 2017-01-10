ec54950d75794cde096f6c095f2cd4536ce7998b
user <user@localhost>
2017-01-10 20:26:49 +0100

    nieuwe pagina admin

---
+++ admin/adminDash.php
@@ -0,0 +1,22 @@
+<?php
+    
+    session_start();
+    require_once 'functies.php'; //Include de functies.
+    require_once 'header.php'; // Zet de header bovenaan deze pagina.
+?>
+
+
+<html>
+    <header>
+        <title> </title>
+    </header>
+    <body>
+        <nav class= "" style="width:25%">
+            <a href="#">Link 1</a><br>
+            <a href="#">Link 2</a><br>
+            <a href="#">Link 3</a><br>
+            <a href="#">Link 4</a><br>
+        </nav>
+
+        </body>  
+</html>    
