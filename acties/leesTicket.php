0505d79c3c3ddf10071f73833fb9b9eab580227b
user <user@localhost>
2017-01-10 15:57:54 +0100

    Frontend: nogBellen

--- acties/leesTicket.php
+++ acties/leesTicket.php
@@ -58,7 +58,7 @@
             
     if($_SESSION['accountNr'] === $ticket['fstAccountNr']){
         echo '
-            <form action="">
+            <form action="leesTicket.php?ticket='. $ticket['ticketId'] .'"method="POST">
             <p><strong> Doorsturingssysteem: </strong></p>';
 
         if($ticket['lijnNr'] > 1) {
@@ -69,7 +69,8 @@
         if($ticket['lijnNr'] < 2) {
             echo '
                 <button name="lijnUp" type="submit" value="lijnUp">Lijnomhoog</button> ';
-            }
+            }            
+
             echo'
                 </form>';
             
@@ -98,7 +99,21 @@
             // De query om de lijn omlaag te werpen
             echo 'Lijnomlaag';
         }
-            
+        
+        echo '<form action="leesTicket.php?ticket='. $ticket['ticketId'] .'" method="POST">';
+        
+        if($ticket['nogBellen'] === 1){
+            echo 'Klant moet nog gebeld worden!
+                <button name="nogBellen" type="submit" value="nogBellen">Klant is gebeld</button>';
+
+        } else {
+            echo '
+                <button name="nogBellen" type="submit" value="nogBellen">Klant moet gebeld worden</button>
+                </form>
+                ';
+        }
+        
+        echo '<form name="leesTicket" action="leesTicket.php?ticket='. $ticket['ticketId'] .'" method="POST">';
       
 ?>
 
@@ -106,54 +121,8 @@
 Je kan hier ook nieuw commentaar aanmaken, en oplossingen aandragen. Door de eerste behandelaar kan die definitief verklaard
 worden. Er kan dus aan een ticket meerdere stukken commentaar en oplossingen hangen! Mocht de echte ticket aangepast worden
 wordt de gebruiker doorgestuurd naar wijzigTicket.php. Dit alleen als er bijvoorbeeld een fout gemaakt is.-->
-<form name="leesTicket" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-       
-          <input type="checkbox" name="nogBellen" value="nogBellen" disabled>Klant moet nog gebeld worden<br><br>
-          
-          <h3> Categorieën </h3>
-          
-          Categorie <!-- Moet uit database komen -->
-          <select name="categorie" disabled>
-              <option>Software</option>
-              <option>Hardware</option>
-          </select><br><br>
-          
-          Subcategorie <!-- Disabled, voor later. -->
-          <select name="subCategorie" disabled>
-              <option>Fedora Linux</option>
-              <option></option>
-          </select><br>
 
-          
-          <h3> Streefdatum</h3>
-          
-                Dag <br>
-                <select name="dag" disabled>
-                <option>1</option>
-                <option>2</option>
-                </select><br><br>
-          
-                Maand <br>
-                <select name="maand" disabled>
-                <option>Januari</option>
-                <option>Februari</option>
-                </select><br><br>
-          
-                Jaar (2017) <br>
-                <input type="text" name="jaar" disabled><br><br>
-
-                     Binnenkomst type: 
-          <select name="binnenkomstType" disabled> <!-- Moet nog gescript worden! Data moet uit database komen -->
-              <option>Telefoon</option>
-              <option>E-mail</option>
-          </select><br><br>
-
-                     Lokatie: 
-              <br><select name="binnenkomstType" disabled><br> <!-- Disabled, gaan we nog niets mee doen-->
-              <option>Hilversum Soestdijkerstraatweg</option>
-              <option></option>
-          </select><br>
-
+                 
           <h3>Veelvoorkomende laptop:</h3><br> <!-- Disabled, weinig tijd -->
           Merk
           <select name="vVLaptopMerk" disabled>
@@ -167,11 +136,6 @@ wordt de gebruiker doorgestuurd naar wijzigTicket.php. Dit alleen als er bijvoor
               <option></option>
           </select><br><br>
           
-          Besturingssysteem
-          <br><select name="besturingssysteem" disabled>
-              <option>Windows</option>
-              <option>Linux</option>
-          </select><br><br>
           
           <h3> Potentiele oplossing </h3>
             <textarea id="oplossing" rows="10" cols="90" ></textarea><br><br>
@@ -188,15 +152,6 @@ wordt de gebruiker doorgestuurd naar wijzigTicket.php. Dit alleen als er bijvoor
             
            <h3> Nieuw Commentaar </h3>
             <textarea id="nieuwComment" rows="10" cols="90"></textarea><br><br>
-
-            <?php
-            
-           if($_SESSION["accountNr"] == $uitkomst['accountNr']) {
-                //echo '<input type="checkbox" name="lijnOmhoog" value="lijnOmhoog" disabled>Ticket moet naar volgende lijn<br><br>';
-                if($uitkomst['lijnNr'] > 1) {
-                    echo '<input type="checkbox" name="lijnOmlaag" value="lijnOmlaag" disabled>Ticket moet naar vorige lijn<br><br>';
-                }
-           }
-            ?>       
+             
           <input type="submit" name="nieuwCommentaar" value="nieuwCommentaar"><br>    
 </form></body></html>
