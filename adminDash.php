0010f22f297c723ba924b72ce565158a6eb5cdcb
user <user@localhost>
2017-01-13 00:42:32 +0100

    nieuwe pagina admin

--- adminDash.php
+++ adminDash.php
@@ -13,7 +13,7 @@
     <body>
         <nav class= "" style="width:100%">
             <a href="admin/accounts.php">Bestaande accounts</a><br><br>
-            <a href="admin/nieuwAccounts.php">Nieuwe accounts</a><br><br>
+            <a href="admin/nieuwAccount.php">Nieuwe accounts</a><br><br>
             <a href="admin/invoerAparaten.php">Apparaten invoer</a><br><br>
             
         </nav>
