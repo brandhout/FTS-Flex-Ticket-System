50d8154d7b7b49037da0ba4f924efc034d4fe0a4
rhuijzer <rhuijzer@localhost>
2017-01-15 15:44:04 +0100

    Laptoptype nu werkend, niet meer stuk, spellingsfout hersteld

--- acties/nieuwTicketNieuwKlant.php
+++ acties/nieuwTicketNieuwKlant.php
@@ -34,11 +34,10 @@ $probleem = $_POST["probleem"];
 $oplossing = $_POST["oplossing"];
 if(isset($_POST['laptopType'])){
     $merktype = leesLaptopTypeId($_POST['laptopType']);
-    $merklaptop = leesLaptopMerkId($merktype);
 } else {
-    $merktype = 0;
     $merklaptop = 0;
 }
+
 $scategorie = $_POST["subCategorie"];
 $besturingsysteem = $_POST["besturingssysteem"];
 $binnenkomstT = $_POST["binnenkomstType"];
