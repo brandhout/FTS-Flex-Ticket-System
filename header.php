60a6408a14b24c9f639592c3790b32dbf8ef904b
user <user@localhost>
2017-01-18 11:13:24 +0100

    v. 17.1.2017 ticket bk: verbetering style/beautified

--- header.php
+++ header.php
@@ -16,11 +16,6 @@
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="styles/js/bootstrap.min.js"></script>
-  <script>
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-  </script>
    <script>
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
