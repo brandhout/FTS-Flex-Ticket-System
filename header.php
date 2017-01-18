348e7e433a71bd8cd48fdaa2e523a51e1c521bb8
user <user@localhost>
2017-01-18 12:54:54 +0100

    v. 17.1.2017 header carousel fixed

--- header.php
+++ header.php
@@ -9,9 +9,13 @@
     <!--bootstrap--> 
   <link rel="stylesheet" type="text/css" href="styles/css/bootstrap.css">
   <script src="styles/js/bootstrap.min.js"></script>
-
-  
+   <!--datepicker -->
+     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="styles/js/bootstrap.min.js"></script>
    <script>
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
