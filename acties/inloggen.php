ca476fe55c519e14e4672e3cb555b1cfa25b45a4
user <user@localhost>
2017-01-19 12:32:08 +0100

    v. 19.1.2017 jQuery tables ingevoegd, knoppen ticketdash veranderd, center form

--- acties/inloggen.php
+++ acties/inloggen.php
@@ -42,8 +42,13 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="../styles.css">
+    <style>
+        body{
+            background-image:url("../back.jpg");
+        }
+    </style>   
     </head>
-    
+ 
     <body>
         <div class='inlogg'>   
         
