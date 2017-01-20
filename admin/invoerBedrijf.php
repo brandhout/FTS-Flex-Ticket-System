e31c88b60248a8836e1240bc2593916a4491d4df
rhuijzer <rhuijzer@localhost>
2017-01-20 12:10:29 +0100

    Invoeren bedrijf

--- admin/invoerBedrijf.php
+++ admin/invoerBedrijf.php
@@ -19,6 +19,42 @@
     session_start();
     require_once '../functies.php'; //Include de functies.
     require_once 'headerUp.php'; // Zet de header bovenaan deze pagina.
+    ini_set('display_erors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $connectie = verbinddatabase();
+    
+    if(isset($_POST['submitBedrijf'])){
+        $naam = $_POST['naam'];
+        $website = $_POST['website'];
+        if(isset($_POST['kvkNr'])){
+            $kvkNr = filter_var($_POST['kvkNr'], FILTER_SANITIZE_NUMBER_INT);
+        } else {
+            $kvkNr = 0;
+        }
+        $adres = $_POST['adres'];
+        $postC = $_POST['postC'];
+        $stad = $_POST['stad'];
+        if(isset($_POST['tel'])){
+            $tel = filter_var($_POST['tel'], FILTER_SANITIZE_NUMBER_INT);
+        } else {
+            $tel = 0;
+        }
+        
+        if(isset($_POST['btwNr'])){
+            $btwNr = filter_var($_POST['btwNr'], FILTER_SANITIZE_NUMBER_INT);
+        } else {
+            $btwNr = 0;
+        }
+        
+        $insertBedrijfQuery = $connectie->prepare("INSERT INTO bedrijf (bedrijfsId, naam, website, kvkNr, 
+                btwNr, adres, stad, postC, tel) VALUES ('', ?, ?,
+                ?, '$btwNr', ?, ?, ?, '$tel')");
+        $insertBedrijfQuery->bind_param("ssssss", $naam, $website, $kvkNr, $adres, $stad, $postC);
+        $insertBedrijfQuery->execute();
+        $insertBedrijfQuery->close();
+        header('url=../index.php');
+    }
 ?>
 
 <html>
@@ -28,21 +64,21 @@
     <body> <strong>
              <form name="bedrijf" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                 Bedrijfsnaam <br>
-                <input type="text" name="naam"><br><br>
+                <input type="text" name="naam" required><br><br>
                 Website <br>
-                <input type="text" name="website"><br><br>
+                <input type="text" name="website" required><br><br>
                 Kamer van Koophandel nummer <br>
                 <input type="text" name="kvkNr"><br><br>
                 BTW. Nummer <br>
                 <input type="text" name="btwNr"><br><br>
                 Adres <br>
-                <input type="text" name="adres"><br><br>
+                <input type="text" name="adres" required><br><br>
                 Postcode <br>
-                <input type="text" name="postC"><br><br>
+                <input type="text" name="postC" required><br><br>
                 Stad <br>
-                <input type="text" name="stad"><br><br>
+                <input type="text" name="stad" required><br><br>
                 Telefoonnummer <br>
-                <input type="text" name="telNr"><br><br></strong>
+                <input type="text" name="tel"><br><br></strong>
                 <button name="submitBedrijf" type="submit" value="1">Invoeren</button>
              </form>
                
