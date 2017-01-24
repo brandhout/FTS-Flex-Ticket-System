daaab4675f754933e5bd3c9bbfdf91743ed590c9
naomiberkelaar <user@localhost>
2017-01-24 21:29:01 +0100

    invoer instantie doet het er zit alleen een foutje in de database, binnenkort nieuwe dump, nu werkt het systeem met die fout!

--- admin/invoerInstantie.php
+++ admin/invoerInstantie.php
@@ -1,24 +1,32 @@
 <?php
 
-/* 
- * Copyright (C) 2017 rhuijzer
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-    session_start();
+   session_start();
     require_once '../functies.php'; //Include de functies.
     require_once '../header.php'; //Include de header.
+    ini_set('display_erors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $connectie = verbinddatabase();
+    
+    if(isset($_POST['submitInstantie'])){
+        
+        $instantie = $_POST['instantie'];
+        //$prioriteit = $_POST['prioriteit'];
+        $prioriteit = filter_var($_POST['prioriteit'], FILTER_SANITIZE_NUMBER_INT);
+        echo $instantie . '<br>' . $prioriteit . '<br>';
+        
+        $insertInstantieQuery = $connectie->prepare("INSERT INTO instantie (instantieId, instantieNaam, proriteit) VALUES ('', ?, '$prioriteit')");
+        echo 'prepare<br>';
+        $insertInstantieQuery->bind_param("s", $instantie);
+        echo 'bind_param<br>';
+
+        $insertInstantieQuery->execute();
+        echo 'execute<br>';
+        $insertInstantieQuery->close();
+        //header('url=adminDash');
+        echo '<br>';
+        echo 'klaar';
+    }
 ?>
 
 
@@ -26,6 +34,19 @@
     <header>
         <title>Admin Invoer FTS</title>
     </header>
-    <body> <strong>
-             <form name="instantie" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                Instantie <br>
+    <body>
+        Invoer instanties<br><br>
+        
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                Naam instantie<br>
+                <input type="text"  name="instantie"><br>
+
+                Prioriteit<br>
+                <input type="text" name="prioriteit"><br><br>
+
+                <button  name="submitInstantie" type="submit" value="1">Invoeren</button>
+            </form>
+    </body>
+</html>    
+    
+                
