88be82db71ad93c0893a24841ea8792e010bce61
rhuijzer <rhuijzer@localhost>
2017-01-27 21:29:27 +0100

    strings escapen

--- admin/nieuwAccount.php
+++ admin/nieuwAccount.php
@@ -25,7 +25,7 @@
                     echo htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="POST"<br>';
 
                     echo '<table cellspacing="0" cellpading="5"width="90%">
-                        <tr><td>Indien leerling: Klassencode<br>
+                        <tr><td>Klassencode (indien leerling)<br>
                         <select name="klassencode">
                         <option value ="">---Select---</option>';
 
@@ -77,21 +77,21 @@
         } else { 
             $admin = 0;
         }
-            
+        
+        $_POST["naam"] = $connectie->escape_string($_POST["naam"]);
+        $_POST["achterNaam"] = $connectie->escape_string($_POST["achterNaam"]);
+        $_POST["gebruikersNaam"] = $connectie->escape_string($_POST["gebruikersNaam"]);
+        $_POST["wachtwoord"] = $connectie->escape_string($_POST["wachtwoord"]);
+        
         $hashin = password_hash($_POST["wachtwoord"], PASSWORD_BCRYPT);
         
         $insertAccount = $connectie->prepare('INSERT INTO account (accountNr, lijnNr, isAdmin, naam, achterNaam, laasteKeerIngelogd, actief, vestigingId, gebruikersNaam, wachtwoord)
-                                 VALUES (0, "'  . $_POST["lijnNr"] . '","' . $admin . '","' . $_POST["naam"] . '","' . $_POST["achterNaam"] . '","' . $datum . '","' . "1" . '","' . $_POST["vestigingId"] . '","' .  $_POST["gebruikersNaam"] . '","' . $_POST["wachtwoord"] . '")');
-                                if ($insertAccount) {
-                                    if ($insertAccount->execute()) {
-                                         header("Refresh:1; url=accounts.php", true, 303);
-                                    }
-                                }
-        
-        
-        
-        echo 'submit uitgevoerd';
-       
+            VALUES (0, "'  . $_POST["lijnNr"] . '","' . $admin . '","' . $_POST["naam"] . '","' . $_POST["achterNaam"] . '","' . $datum . '","' . "1" . '","' . $_POST["vestigingId"] . '","' .  $_POST["gebruikersNaam"] . '","' . $_POST["wachtwoord"] . '")');
+        if ($insertAccount) {
+            if ($insertAccount->execute()) {
+                header("Refresh:1; url=accounts.php", true, 303);
+            }
+        }
     }
     ?>
 
