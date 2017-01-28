fcb0f402719da46144da77d246779454cc67af9f
naomiberkelaar <user@localhost>
2017-01-28 14:31:12 +0100

    download functie werkt

--- acties/leesBijlage.php
+++ acties/leesBijlage.php
@@ -9,4 +9,19 @@ session_start();
     //ini_set('display_errors', 1);
     //ini_set('display_startup_errors', 1);
     //error_reporting(E_ALL);
+    $connectie = verbinddatabase();
+
+    if(isset($_GET['id'])){
+        $id    = $_GET['id'];   
+        $bijlage2Query = "SELECT naam, type, bijlage FROM bijlage WHERE id = '$id'";
+        $bijlage2Uitkomst = $connectie->query($bijlage2Query);
+        while($bijlage2 = $bijlage2Uitkomst->fetch_assoc()){
+            $naam = $bijlage2['naam'];
+            $type = $bijlage2['type'];
+            $bijlage = $bijlage2['bijlage'];
+            echo '<img src="data:' . $type . ';base64,'. base64_encode( $bijlage ) . '"/>';
+         }
+ 
+    }
+    
 ?>
