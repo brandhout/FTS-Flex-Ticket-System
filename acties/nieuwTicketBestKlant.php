87fbda7a12e1721afe033c47adfe0ecc5cea09b4
rhuijzer <rhuijzer@localhost>
2017-02-01 14:50:51 +0100

    Nutteloze code weg

--- acties/nieuwTicketBestKlant.php
+++ acties/nieuwTicketBestKlant.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-require_once 'AJAX/zoekKlant.php';
 require_once '../functies.php'; //Include de functies.
 ini_set('display_erors', 1);
 ini_set('display_startup_errors', 1);
