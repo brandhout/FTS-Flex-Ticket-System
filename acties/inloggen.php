0fd5e66f324cb9c2770206e3c8667788296edbea
rhuijzer <rhuijzer@localhost>
2017-03-10 09:45:51 +0100

    Include van style gefixt

--- acties/inloggen.php
+++ acties/inloggen.php
@@ -37,7 +37,7 @@
 <!-- inloggen -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../styles.css">
+        <link rel="stylesheet" href="../styles/styles.css">
         <style>
             body{
                 background-image:url("../back.jpg");
