3860a0ca85d24fbd5a9f49aab5f54cf58dcef06a
rhuijzer <rhuijzer@localhost>
2017-03-10 12:01:08 +0100

    Proof of concept INDEX

--- index.php
+++ index.php
@@ -40,11 +40,63 @@ $datum = new DateTime();
 
 $gebruiker = $_SESSION["gebruiker"];
 
+$achternaam = leesAccountAchterNaam($gebruiker->getAccountNr());
+
 ?>
 
 <!DOCTYPE html>
-      <html>
-      <head>
-          <meta http-equiv="Refresh" content="60">
-          <title>Flex Ticket System</title>
-      </head>
+    <html>
+    <head>
+        <meta http-equiv="Refresh" content="60">
+        <title>Flex Ticket System</title>
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+        <style>
+            body{
+                background-image:url("back.jpg");
+            }
+        
+        </style>   
+
+    </head>
+    <body>
+    <div class="container w3-container w3-dark-grey" style="text-align:center;">
+    
+        <h2>Welkom, <?php echo $achternaam?> </h2>
+        <div class="nieuwDataBar w3-teal w3-hover-shadow w3-padding-64 w3-center">
+            <i class="fa fa-pencil-square-o fa-5x"></i>
+            <p> Openstaande Tickets </p>
+            <h1> X </h1>
+        </div>
+        
+        <div class="nieuwDataBar w3-green w3-hover-shadow w3-padding-64 w3-center">
+            <i class="fa fa-ticket fa-5x"></i>
+            <p> Aantal Tickets </p>
+            <h1> X </h1>
+        </div>
+
+        <div class="nieuwDataBar w3-indigo w3-hover-shadow w3-padding-64 w3-center">
+            <i class="fa fa-times fa-5x"></i>
+            <p> Gesloten Tickets </p>
+            <h1> X </h1>
+        </div>
+        <h3 style="text-align:center;"> Openstaande Tickets: </h3>
+        
+        <div class=" ticketCard w3-card-8 w3-green w3-hover-shadow w3-padding-64 w3-center">
+            <div class="w3-container w3-center">
+                <h3>Ticket</h3>
+            </div>
+            <p>
+            <i class="fa fa-ticket fa-5x"></i><strong>1</strong><p>
+            <p> 
+            Info: 66 
+            Info: 99 
+            Info: 99
+            <div class="w3-section">
+                <button class="w3-button w3-grey">Openen</button>
+            </div>
+            
+            </p></div>
+
+        
+    </div>
+        
