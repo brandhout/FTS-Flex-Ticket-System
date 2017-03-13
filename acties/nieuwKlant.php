43a599b456fa8e9f3f68ff1cb32a0061483aafbb
rhuijzer <rhuijzer@localhost>
2017-03-13 20:41:52 +0100

    CSS update

--- acties/nieuwKlant.php
+++ acties/nieuwKlant.php
@@ -22,7 +22,8 @@ require_once '../functies.php'; //Include de functies.
 $connectie = verbinddatabase();
 
 if($_GET['popup'] == 1){
-    echo'<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>';
+    echo'<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>'
+    . '<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">';
 }
 
 
@@ -64,6 +65,7 @@ if (isset($_POST['nieuwKlant'])){
     }
 }
 ?>
+<head>
         <script>
             function bedrijf(){
                 var zoektxt = $("input[name='zoekBedrijf']").val();
@@ -72,54 +74,43 @@ if (isset($_POST['nieuwKlant'])){
                 });             
                 }
         </script>
-        
+</head>
+<body>
  
-        <div class="container">
-            <div class="inner contact">
+        <div class="w3-container">
                 <!-- Form Area -->
-                <div class="contact-form">
                     <!-- Form -->
-                    <form name="nieuwTicket" action="nieuwKlant.php" method="POST">
-                        <!-- Left Inputs -->
-			<div class="grid">
-			<div class="row">
-                        <div class="col-sm-3 wow animated slideInLeft" data-wow-delay=".5s">
-                            <!-- voornaam -->
-                            <input type="text" name="klantNaam" id="name" required="required" class="form" placeholder="voornaam" /><br>
-                            <!-- achternaam -->
-                            <input type="text" name="klantAchternaam" id="lname" required="required" class="form" placeholder="achternaam" /><br>
-                            <!-- adres -->
-                            <input type="text" name="klantAdres" id="adres" required="required" class="form" placeholder="adres" /><br>
-                            <!-- postcode -->
-                            <input type="text" name="klantPostc" id="zipcode" required="required" class="form" placeholder="postcode" /><br>
-                            <!-- woonplaats -->
-                            <input type="text" name="klantStad" id="city" required="required" class="form" placeholder="woonplaats" /><br>	
-                            <!-- telefoonnummer -->
-                            <input type="text" name="klantTel" id="phone" required="required" class="form" placeholder="telefoonnummer" /><br>	
-                            <!-- email -->
-                            <input type="email" name="klantEmail" id="email" required="required" class="form" placeholder="E-mail" /><br>							
-                        </div>
-                        <!-- End Left Inputs -->
-                        <!-- mid inputs -->
-                            <div class="col-sm-3 wow animated slideInLeft" data-wow-delay=".5s">
-                            <select class="form" name="instantie">
-                            <option value = "">---instanties---</option>';
+                    <form name="nieuwTicket" action="nieuwKlant.php" method="POST" class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin">
+                        <h2 class="w3-center">Nieuwe Klant</h2>
+                        <!-- voornaam -->
+                        <input type="text" name="klantNaam" id="name" required="required" class="w3-input w3-border" placeholder="voornaam" /><br>
+                        <!-- achternaam -->
+                        <input type="text" name="klantAchternaam" id="lname" required="required" class="w3-input w3-border" placeholder="achternaam" /><br>
+                        <!-- adres -->
+                        <input type="text" name="klantAdres" id="adres" required="required" class="w3-input w3-border" placeholder="adres" /><br>
+                        <!-- postcode -->
+                        <input type="text" name="klantPostc" id="zipcode" required="required" class="w3-input w3-border" placeholder="postcode" /><br>
+                        <!-- woonplaats -->
+                        <input type="text" name="klantStad" id="city" required="required" class="w3-input w3-border" placeholder="woonplaats" /><br>	
+                        <!-- telefoonnummer -->
+                        <input type="text" name="klantTel" id="phone" required="required" class="w3-input w3-border" placeholder="telefoonnummer" /><br>	
+                        <!-- email -->
+                        <input type="email" name="klantEmail" id="email" required="required" class="w3-input w3-border" placeholder="E-mail" /><br>							
+                        <select name="instantie" class="w3-input w3-border">
+                        <option value = "">---instanties---</option>';
 
-                            <?php
-                            $ophaali = "SELECT * FROM instantie ";
-                            $resulti = mysqli_query($connectie, $ophaali);
-                            while ($l = mysqli_fetch_assoc($resulti)) {
-                            echo "<option value='" . $l['instantieId'] . "'>" . $l['instantieNaam'] . "</option>";
-                            }
-                            ?> 
+                        <?php
+                        $ophaali = "SELECT * FROM instantie ";
+                        $resulti = mysqli_query($connectie, $ophaali);
+                        while ($l = mysqli_fetch_assoc($resulti)) {
+                        echo "<option value='" . $l['instantieId'] . "'>" . $l['instantieNaam'] . "</option>";
+                        }
+                        ?> 
 
-                            </select><br>						
-                            <input type="text" name="zoekBedrijf" id="zoekBedrijf" class="form" onblur="bedrijf();" placeholder="zoek op bedrijfsnaam" />
-                            <p type="text" class="form" id="bedrijfsnaam" name="bedrijfsnaam" placeholder="resultaat"></p>
-                            
-                            <!-- End Mid Inputs -->
-                            </div>
-                            
-                            <div class="relative fullwidth col-xs-12">
-                            <button type="submit" id="nieuwKlant" name="nieuwKlant" class="form-btn semibold">invoeren</button>
-                            </div></form></body></html>                                                      
+                        </select><br>						
+                        <input type="text" name="zoekBedrijf" id="zoekBedrijf" onblur="bedrijf();" class="w3-input w3-border" placeholder="zoek op bedrijfsnaam" />
+                        <p type="text" id="bedrijfsnaam" name="bedrijfsnaam" placeholder="resultaat"></p>
+
+                        <p class="w3-center">
+                        <button type="submit" class="w3-button w3-section w3-blue w3-ripple" id="nieuwKlant" name="nieuwKlant" class="form-btn semibold">invoeren</button>
+        <p></form></div></body></html>                                                      
